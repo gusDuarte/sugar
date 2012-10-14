9677e9fd273963ecd7be54022d40340c89c2a0c4
Ajay Garg <ajay@activitycentral.com>
2012-10-14 23:51:26 +0530

    3G-Network sharing.
    Signed-off-by: Ajay Garg <ajay@activitycentral.com>

--- src/jarabe/model/network.py
+++ src/jarabe/model/network.py
@@ -910,12 +910,14 @@ def activate_connection_by_path(connection, device_o,
                                      error_handler=error_handler)
 
 
-def add_and_activate_connection(device_o, settings, specific_object):
+def add_and_activate_connection(device_o, settings, specific_object,
+                                reply_handler=_add_and_activate_reply_cb,
+                                error_handler=_add_and_activate_error_cb):
     manager = get_manager()
     manager.AddAndActivateConnection(settings.get_dict(), device_o,
                                      specific_object,
-                                     reply_handler=_add_and_activate_reply_cb,
-                                     error_handler=_add_and_activate_error_cb)
+                                     reply_handler=reply_handler,
+                                     error_handler=error_handler)
 
 
 def _migrate_old_wifi_connections():
