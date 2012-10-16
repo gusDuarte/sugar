69d42bea9e1bb8cda72dbec350db84c3f651f21c
Ajay Garg <ajay@activitycentral.com>
2012-10-16 23:18:35 +0530

    Revert "Notifications (gconf-controllable)" Soon to commit version-2 of this !!!
    This reverts commit 73c3c268dff68e5d3a6d9701418c2a8b72861150.

--- src/jarabe/frame/frame.py
+++ src/jarabe/frame/frame.py
@@ -15,7 +15,6 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 import logging
-import os
 
 from gi.repository import Gtk
 from gi.repository import Gdk
@@ -34,7 +33,6 @@ from jarabe.frame.devicestray import DevicesTray
 from jarabe.frame.framewindow import FrameWindow
 from jarabe.frame.clipboardpanelwindow import ClipboardPanelWindow
 from jarabe.frame.notification import NotificationIcon, NotificationWindow
-from jarabe.frame.notification import NotificationButton, HistoryPalette
 from jarabe.model import notifications
 
 
@@ -44,8 +42,6 @@ BOTTOM_RIGHT = 2
 BOTTOM_LEFT = 3
 
 _NOTIFICATION_DURATION = 5000
-
-_DEFAULT_ICON = 'emblem-notification'
 
 
 class _Animation(animator.Animation):
@@ -87,10 +83,6 @@ class Frame(object):
         self._event_area.connect('enter', self._enter_corner_cb)
         self._event_area.show()
 
-        self._activities_tray = None
-        self._devices_tray = None
-        self._friends_tray = None
-
         self._top_panel = self._create_top_panel()
         self._bottom_panel = self._create_bottom_panel()
         self._left_panel = self._create_left_panel()
@@ -102,7 +94,6 @@ class Frame(object):
         self._key_listener = _KeyListener(self)
 
         self._notif_by_icon = {}
-        self._notif_by_message = {}
 
         notification_service = notifications.get_service()
         notification_service.notification_received.connect(
@@ -152,8 +143,6 @@ class Frame(object):
         panel.append(activities_tray)
         activities_tray.show()
 
-        self._activities_tray = activities_tray
-
         return panel
 
     def _create_bottom_panel(self):
@@ -163,8 +152,6 @@ class Frame(object):
         panel.append(devices_tray)
         devices_tray.show()
 
-        self._devices_tray = devices_tray
-
         return panel
 
     def _create_right_panel(self):
@@ -173,8 +160,6 @@ class Frame(object):
         tray = FriendsTray()
         panel.append(tray)
         tray.show()
-
-        self._friends_tray = tray
 
         return panel
 
@@ -226,7 +211,15 @@ class Frame(object):
         else:
             self.show()
 
-    def _create_notification_window(self, corner):
+    def notify_key_press(self):
+        self._key_listener.key_press()
+
+    def add_notification(self, icon, corner=Gtk.CornerType.TOP_LEFT,
+                         duration=_NOTIFICATION_DURATION):
+
+        if not isinstance(icon, NotificationIcon):
+            raise TypeError('icon must be a NotificationIcon.')
+
         window = NotificationWindow()
 
         screen = Gdk.Screen.get_default()
@@ -241,47 +234,6 @@ class Frame(object):
                         screen.get_height() - style.GRID_CELL_SIZE)
         else:
             raise ValueError('Inalid corner: %r' % corner)
-
-        return window
-
-    def _add_message_button(self, button, corner):
-        if corner == Gtk.CornerType.BOTTOM_RIGHT:
-            self._devices_tray.add_item(button)
-        elif corner == Gtk.CornerType.TOP_RIGHT:
-            self._friends_tray.add_item(button)
-        else:
-            self._activities_tray.add_item(button)
-
-    def _remove_message_button(self, button, corner):
-        if corner == Gtk.CornerType.BOTTOM_RIGHT:
-            self._devices_tray.remove_item(button)
-        elif corner == Gtk.CornerType.TOP_RIGHT:
-            self._friends_tray.remove_item(button)
-        else:
-            self._activities_tray.remove_item(button)
-
-    def _launch_notification_icon(self, icon_name, xo_color,
-                                  position, duration):
-        icon = NotificationIcon()
-        icon.props.xo_color = xo_color
-
-        if icon_name.startswith(os.sep):
-            icon.props.icon_filename = icon_name
-        else:
-            icon.props.icon_name = icon_name
-
-        self.add_notification(icon, position, duration)
-
-    def notify_key_press(self):
-        self._key_listener.key_press()
-
-    def add_notification(self, icon, corner=Gtk.CornerType.TOP_LEFT,
-                         duration=_NOTIFICATION_DURATION):
-
-        if not isinstance(icon, NotificationIcon):
-            raise TypeError('icon must be a NotificationIcon.')
-
-        window = self._create_notification_window(corner)
 
         window.add(icon)
         icon.show()
@@ -301,76 +253,28 @@ class Frame(object):
         window.destroy()
         del self._notif_by_icon[icon]
 
-    def add_message(self, body, summary='', icon_name=_DEFAULT_ICON,
-                    xo_color=None, corner=Gtk.CornerType.TOP_LEFT,
-                    duration=_NOTIFICATION_DURATION):
-
-        if xo_color is None:
-            xo_color = profile.get_color()
-
-        button = self._notif_by_message.get(corner, None)
-        if button is None:
-            button = NotificationButton(_DEFAULT_ICON, xo_color)
-            button.show()
-            self._add_message_button(button, corner)
-            self._notif_by_message[corner] = button
-
-        palette = button.get_palette()
-        if palette is None:
-            palette = HistoryPalette()
-            palette.set_group_id('frame')
-            palette.connect('clear-messages', self.remove_message, corner)
-            palette.connect('notice-messages', button.stop_pulsing)
-            button.set_palette(palette)
-
-        button.start_pulsing()
-
-        palette.push_message(body, summary, icon_name, xo_color)
-        self._launch_notification_icon(_DEFAULT_ICON, xo_color, corner, duration)
-
-    def remove_message(self, palette, corner):
-        if corner not in self._notif_by_message:
-            logging.debug('Button %s is not active', str(corner))
-            return
-
-        button = self._notif_by_message[corner]
-        self._remove_message_button(button, corner)
-        del self._notif_by_message[corner]
-
     def __notification_received_cb(self, **kwargs):
-        logging.debug('__notification_received_cb %r', kwargs)
+        logging.debug('__notification_received_cb')
+        icon = NotificationIcon()
 
         hints = kwargs['hints']
 
-        icon_name = hints.get('x-sugar-icon-file-name', '')
-        if not icon_name:
-            icon_name = _DEFAULT_ICON
+        icon_file_name = hints.get('x-sugar-icon-file-name', '')
+        if icon_file_name:
+            icon.props.icon_filename = icon_file_name
+        else:
+            icon.props.icon_name = 'application-octet-stream'
 
         icon_colors = hints.get('x-sugar-icon-colors', '')
         if not icon_colors:
             icon_colors = profile.get_color()
+        icon.props.xo_color = icon_colors
 
         duration = kwargs.get('expire_timeout', -1)
         if duration == -1:
             duration = _NOTIFICATION_DURATION
 
-        category = hints.get('category', '')
-        if category == 'device':
-            position = Gtk.CornerType.BOTTOM_RIGHT
-        elif category == 'presence':
-            position = Gtk.CornerType.TOP_RIGHT
-        else:
-            position = Gtk.CornerType.TOP_LEFT
-
-        summary = kwargs.get('summary', '')
-        body = kwargs.get('body', '')
-
-        if summary or body:
-            self.add_message(body, summary, icon_name,
-                            icon_colors, position, duration)
-        else:
-            self._launch_notification_icon(icon_name, icon_colors,
-                                          position, duration)
+        self.add_notification(icon, Gtk.CornerType.TOP_RIGHT, duration)
 
     def __notification_cancelled_cb(self, **kwargs):
         # Do nothing for now. Our notification UI is so simple, there's no
--- src/jarabe/frame/notification.py
+++ src/jarabe/frame/notification.py
@@ -18,196 +18,10 @@ from gi.repository import GObject
 from gi.repository import Gtk
 from gi.repository import Gdk
 
-import re
-import os
-
-from gettext import gettext as _
-
 from sugar3.graphics import style
 from sugar3.graphics.xocolor import XoColor
-from sugar3.graphics.palette import Palette
-from sugar3.graphics.palettemenuitem import PaletteMenuItem
-from sugar3.graphics.toolbutton import ToolButton
-from sugar3 import profile
-
-from jarabe.frame.frameinvoker import FrameWidgetInvoker
 
 from jarabe.view.pulsingicon import PulsingIcon
-
-_PULSE_TIMEOUT = 3
-_PULSE_COLOR = XoColor('%s,%s' % \
-        (style.COLOR_BUTTON_GREY.get_svg(), style.COLOR_TRANSPARENT.get_svg()))
-_BODY_FILTERS = "<img.*?/>"
-
-
-def _create_pulsing_icon(icon_name, xo_color, timeout=None):
-    icon = PulsingIcon(
-            pixel_size=style.STANDARD_ICON_SIZE,
-            pulse_color=_PULSE_COLOR,
-            base_color=xo_color
-            )
-
-    if timeout is not None:
-        icon.timeout = timeout
-
-    if icon_name.startswith(os.sep):
-        icon.props.file = icon_name
-    else:
-        icon.props.icon_name = icon_name
-
-    return icon
-
-
-class _HistoryIconWidget(Gtk.Alignment):
-    __gtype_name__ = 'SugarHistoryIconWidget'
-
-    def __init__(self, icon_name, xo_color):
-        icon = _create_pulsing_icon(icon_name, xo_color, _PULSE_TIMEOUT)
-        icon.props.pulsing = True
-
-        Gtk.Alignment.__init__(self, xalign=0.5, yalign=0.0)
-        self.props.top_padding = style.DEFAULT_PADDING
-        self.set_size_request(
-                style.GRID_CELL_SIZE - style.FOCUS_LINE_WIDTH * 2,
-                style.GRID_CELL_SIZE - style.DEFAULT_PADDING)
-        self.add(icon)
-
-
-class _HistorySummaryWidget(Gtk.Alignment):
-    __gtype_name__ = 'SugarHistorySummaryWidget'
-
-    def __init__(self, summary):
-        summary_label = Gtk.Label()
-        summary_label.props.wrap = True
-        summary_label.set_markup(
-                '<b>%s</b>' % GObject.markup_escape_text(summary))
-
-        Gtk.Alignment.__init__(self, xalign=0.0, yalign=1.0)
-        self.props.right_padding = style.DEFAULT_SPACING
-        self.add(summary_label)
-
-
-class _HistoryBodyWidget(Gtk.Alignment):
-    __gtype_name__ = 'SugarHistoryBodyWidget'
-    def __init__(self, body):
-        body_label = Gtk.Label()
-        body_label.props.wrap = True
-        body_label.set_markup(body)
-
-        Gtk.Alignment.__init__(self, xalign=0, yalign=0.0)
-        self.props.right_padding = style.DEFAULT_SPACING
-        self.add(body_label)
-
-
-class _MessagesHistoryBox(Gtk.VBox):
-    __gtype_name__ = 'SugarMessagesHistoryBox'
-
-    def __init__(self):
-        Gtk.VBox.__init__(self)
-        self._setup_links_style()
-
-    def _setup_links_style(self):
-        # XXX: find a better way to change style for upstream
-        link_color = profile.get_color().get_fill_color()
-        visited_link_color = profile.get_color().get_stroke_color()
-
-        links_style='''
-        style "label" {
-          GtkLabel::link-color="%s"
-          GtkLabel::visited-link-color="%s"
-        }
-        widget_class "*GtkLabel" style "label"
-        ''' % (link_color, visited_link_color)
-        Gtk.rc_parse_string(links_style)
-
-    def push_message(self, body, summary, icon_name, xo_color):
-        entry = Gtk.HBox()
-
-        icon_widget = _HistoryIconWidget(icon_name, xo_color)
-        entry.pack_start(icon_widget, False, False, 0)
-
-        message = Gtk.VBox()
-        message.props.border_width = style.DEFAULT_PADDING
-        entry.pack_start(message, True, True, 0)
-
-        if summary:
-            summary_widget = _HistorySummaryWidget(summary)
-            message.pack_start(summary_widget, False, False, 0)
-
-        body = re.sub(_BODY_FILTERS, '', body)
-
-        if body:
-            body_widget = _HistoryBodyWidget(body)
-            message.pack_start(body_widget, True, True, 0)
-
-        entry.show_all()
-        self.pack_start(entry, True, True, 0)
-        self.reorder_child(entry, 0)
-
-        self_width_ = self.props.width_request
-        self_height = self.props.height_request
-        if (self_height > Gdk.Screen.height() / 4 * 3) and \
-                (len(self.get_children()) > 1):
-            self.remove(self.get_children()[-1])
-
-class HistoryPalette(Palette):
-    __gtype_name__ = 'SugarHistoryPalette'
-
-    __gsignals__ = {
-        'clear-messages': (GObject.SignalFlags.RUN_FIRST, None, ([])),
-        'notice-messages': (GObject.SignalFlags.RUN_FIRST, None, ([]))
-    }
-
-    def __init__(self):
-        Palette.__init__(self)
-
-        self._update_accept_focus()
-
-        self._messages_box = _MessagesHistoryBox()
-        self._messages_box.show()
-
-        palette_box = self._palette_box
-        primary_box = self._primary_box
-        primary_box.hide()
-        palette_box.add(self._messages_box)
-        palette_box.reorder_child(self._messages_box, 0)
-
-        clear_option = PaletteMenuItem(_('Clear history'), 'dialog-cancel')
-        clear_option.connect('activate', self.__clear_messages_cb)
-        clear_option.show()
-
-        vbox = Gtk.VBox()
-        self.set_content(vbox)
-        vbox.show()
-
-        vbox.add(clear_option)
-
-        self.connect('popup', self.__notice_messages_cb)
-
-    def __clear_messages_cb(self, clear_option):
-        self.emit('clear-messages')
-
-    def __notice_messages_cb(self, palette):
-        self.emit('notice-messages')
-
-    def push_message(self, body, summary, icon_name, xo_color):
-        self._messages_box.push_message(body, summary, icon_name, xo_color)
-
-
-class NotificationButton(ToolButton):
-
-    def __init__(self, icon_name, xo_color):
-        ToolButton.__init__(self)
-        self._icon = _create_pulsing_icon(icon_name, xo_color)
-        self.set_icon_widget(self._icon)
-        self._icon.show()
-        self.set_palette_invoker(FrameWidgetInvoker(self))
-
-    def start_pulsing(self):
-        self._icon.props.pulsing = True
-
-    def stop_pulsing(self, widget):
-        self._icon.props.pulsing = False
 
 
 class NotificationIcon(Gtk.EventBox):
@@ -219,29 +33,28 @@ class NotificationIcon(Gtk.EventBox):
         'icon-filename': (str, None, None, None, GObject.PARAM_READWRITE),
     }
 
+    _PULSE_TIMEOUT = 3
+
     def __init__(self, **kwargs):
         self._icon = PulsingIcon(pixel_size=style.STANDARD_ICON_SIZE)
         Gtk.EventBox.__init__(self, **kwargs)
         self.props.visible_window = False
-        self.set_app_paintable(True)
 
-        color = Gdk.color_parse(style.COLOR_BLACK.get_html())
-        self.modify_bg(Gtk.StateType.PRELIGHT, color)
-
-        color = Gdk.color_parse(style.COLOR_BUTTON_GREY.get_html())
-        self.modify_bg(Gtk.StateType.ACTIVE, color)
-
-        self._icon.props.pulse_color = _PULSE_COLOR
-        self._icon.props.timeout = _PULSE_TIMEOUT
+        self._icon.props.pulse_color = \
+                XoColor('%s,%s' % (style.COLOR_BUTTON_GREY.get_svg(),
+                                   style.COLOR_TRANSPARENT.get_svg()))
+        self._icon.props.pulsing = True
         self.add(self._icon)
         self._icon.show()
 
-        self.start_pulsing()
+        GObject.timeout_add_seconds(self._PULSE_TIMEOUT,
+                                    self.__stop_pulsing_cb)
 
         self.set_size_request(style.GRID_CELL_SIZE, style.GRID_CELL_SIZE)
 
-    def start_pulsing(self):
-        self._icon.props.pulsing = True
+    def __stop_pulsing_cb(self):
+        self._icon.props.pulsing = False
+        return False
 
     def do_set_property(self, pspec, value):
         if pspec.name == 'xo-color':
@@ -274,13 +87,17 @@ class NotificationIcon(Gtk.EventBox):
 class NotificationWindow(Gtk.Window):
     __gtype_name__ = 'SugarNotificationWindow'
 
-    def __init__(self):
-        Gtk.Window.__init__(self)
+    def __init__(self, **kwargs):
+
+        Gtk.Window.__init__(self, **kwargs)
 
         self.set_decorated(False)
         self.set_resizable(False)
         self.connect('realize', self._realize_cb)
 
     def _realize_cb(self, widget):
+        self.set_type_hint(Gdk.WindowTypeHint.DIALOG)
+        self.window.set_accept_focus(False)
+
         color = Gdk.color_parse(style.COLOR_TOOLBAR_GREY.get_html())
         self.modify_bg(Gtk.StateType.NORMAL, color)
